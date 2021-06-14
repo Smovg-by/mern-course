ca5a2f328acb23ea4dbc4439734a58c96c712756
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-06-14 19:48:00 +0300

    init: MERN project start

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+// подключаем базу данных mongoDB
+async function start () {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+  } catch (error) {
+    console.log('Server Error', error.message)
+    process.exit(1)
+  }
+}
+
+start() // вызываем функцию подключения БД
+
+app.listen(5000, console.log(`App has been started on ${PORT}`))
